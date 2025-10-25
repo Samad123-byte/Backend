b0e62052f50aa93b7caa1de849c2b0a6663c3cf6
samad <abdulsammadk5@gmail.com>
2025-10-25 16:14:25 +0500

    Create PaginatedResponse.cs

---
+++ Models/PaginatedResponse.cs
@@ -0,0 +1,11 @@
+namespace Backend.Models
+{
+    public class PaginatedResponse<T>
+    {
+        public List<T> Data { get; set; } = new List<T>();
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalRecords { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
