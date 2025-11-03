84cdf23fc96ab7233a151e2eb7229d4f59e3c82f
samad <abdulsammadk5@gmail.com>
2025-11-03 19:34:27 +0500

    Update DeleteRequest.cs

--- Models/DeleteRequest.cs
+++ Models/DeleteRequest.cs
@@ -1,1 +1,11 @@
 
+namespace Backend.Models
+{
+    public class DeleteRequest
+    {
+        public int Id { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public int Code { get; set; } // 1=Deleted, 0=NotFound, -1=FK conflict
+
+    }
+}
